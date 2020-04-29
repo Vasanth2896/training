144dbdd145904286bfb4207128661f55fbd05a22
vasanth_nayak <vasanth@vhitechlab.com>
2020-04-29 15:10:31 +0530

    Vasanth | Completed Exercise-7

--- Exercise-7/exercise7.js
+++ Exercise-7/exercise7.js
@@ -14,8 +14,15 @@ let board = [
     ["d", "e", "a", "d", "l", "i", "n", "e"]
 ];
 let existingWords = [];
-let nonExistingWords = [];
 let callCount = 0;
+let Ylength = board.length;
+let Xlength = board[0].length;
+let maxLength = Math.max(Xlength, Ylength);
+let bottomToTop = true;
+let k;
+
+document.getElementById("showBoard").innerHTML = JSON.stringify(board);
+
 let transpose = () => {
     for (let row = 0; row < board.length; row++) {
         for (let column = 0; column < row; column++) {
@@ -25,7 +32,6 @@ let transpose = () => {
         }
     }
 }
-
 
 getUserInput.onclick = () => {
     if (input.value !== "") {
@@ -48,20 +54,19 @@ output.onclick = () => {
 }
 
 function traverse(board) {
-    checkCommon(board, inputArray);
+    checkHorizontallyAndVertically(board, inputArray);
     checkDiagonally(board, inputArray);
     console.log(`These are the existing words:${existingWords}`, `These are the non existing words:${inputArray}`);
 }
 
-function checkCommon(board, inputArray) {
-    debugger;
+function checkHorizontallyAndVertically(board, inputArray) {
     if (inputArray.length !== 0) {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < inputArray.length; j++) {
                 if (board[i].join("").includes(inputArray[j])) {
                     existingWords.push(inputArray[j]);
                     inputArray.splice(j, 1);
-                    checkCommon(board, inputArray);
+                    checkHorizontallyAndVertically(board, inputArray);
                     return;
                 }
             }
@@ -70,54 +75,47 @@ function checkCommon(board, inputArray) {
     callCount++;
     transpose();
     if (callCount <= 1) {
-        checkCommon(board, inputArray);
+        checkHorizontallyAndVertically(board, inputArray);
+    }
+}
+
+function compareInputDiagonally(temp) {
+
+    let baseComparisonString = temp.join("");
+    for (let iterate = 0; iterate < inputArray.length; iterate++) {
+        if (baseComparisonString.includes(inputArray[iterate])) {
+            existingWords.push(inputArray[iterate]);
+            inputArray.splice(iterate, 1);
+            checkDiagonally(board, inputArray);
+            return;
+        }
+    }
+    if (k === (2 * maxLength) - 1) {
+        bottomToTop = false;
+        checkDiagonally(board, inputArray);
     }
 }
 
 function checkDiagonally(board, inputArray) {
     if (inputArray.length !== 0) {
-        for (let k = 0; k <= board.length - 1; k++) {
-            let i = k, j = 0;
-            let temp = [];
-            while (i >= 0) {
-                temp.push(board[i][j]);
-                if (i === 0) {
-                    let stringTest = temp.join("");
-                    for (let iterate = 0; iterate < inputArray.length; iterate++) {
-                        if (stringTest.includes(inputArray[iterate])) {
-                            existingWords.push(inputArray[iterate]);
-                            inputArray.splice(iterate, 1);
-                            checkDiagonally(board, inputArray);
-                            return;
-                        }
+        let temp;
+        for (k = 0; k <= 2 * maxLength; ++k) {
+            temp = [];
+            for (let y = Ylength - 1; y >= 0; --y) {
+                let x = k - (bottomToTop ? Ylength - y : y);
+                if (x >= 0 && x < Xlength) {
+                    temp.push(board[y][x]);
+                    if (bottomToTop && (x === 0 || y === 0)) {
+                        compareInputDiagonally(temp);
                     }
-                }
-                i--;
-                j++;
-            }
-        }
-        for (let k = 1; k <= board[7].length - 1; k++) {
-            let i = board.length - 1, j = k;
-            let temp = [];
-            while (j <= board[7].length - 1) {
-                temp.push(board[i][j]);
-                if (i === 0) {
-                    let stringTest = temp.join("");
-                    for (let iterate = 0; iterate < inputArray.length; iterate++) {
-                        if (stringTest.includes(inputArray[iterate])) {
-                            existingWords.push(inputArray[iterate]);
-                            existingWords.splice(iterate, 1);
-                            checkDiagonally(board, inputArray);
-                            return;
-                        }
+                    else if (!bottomToTop && (x === 7 || y === 0)) {
+                        compareInputDiagonally(temp);
                     }
+
                 }
-                i--;
-                j++;
             }
         }
     }
-    // console.log(existingWords, inputArray);
 }
 
 
